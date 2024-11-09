83b82005707e074ce6102def46823e950147b4f5
Tommy Woo <mail.tommy.woo@gmail.com>
2024-11-09 15:17:08 -0800

    test 3

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 // hello.js
 function sayHello() {
   const appDiv = document.getElementById("app");
-  appDiv.innerHTML = "Hello hellkkkkko!";
+  appDiv.innerHTML = "Hello testing!";
 }
 
 export default sayHello;
